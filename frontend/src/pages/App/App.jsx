3a7b3a005c327ac330bb1caa92ac072490bed010
johntrinhvu <johntrinhvu@yahoo.com>
2025-01-24 22:29:40 -0800

    downgrade tailwind version

--- frontend/src/pages/App/App.jsx
+++ frontend/src/pages/App/App.jsx
@@ -1,9 +1,7 @@
-import './App.css';
-
 export default function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="font-bold text-3xl">Hello World</h1>
     </div>
   );
 }
